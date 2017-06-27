0f9dee2dabd7ee24f30768b10e09615548c00a1f
liubingguang <liubingguang.sh@superjia.com>
2017-06-27 10:59:53 +0800

    运行时常量池OOM

--- src/main/java/oom/RuntimeConstantPoolOOM.java
+++ src/main/java/oom/RuntimeConstantPoolOOM.java
@@ -1,5 +1,7 @@
 package oom;
 
+import org.junit.Test;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +19,14 @@ public class RuntimeConstantPoolOOM {
         while (true) {
             list.add(String.valueOf(i++).intern());
         }
+    }
+
+    @Test
+    public void test() {
+        String str1 = new StringBuilder("计算机").append("软件").toString();
+        System.out.println(str1.intern() == str1);
+
+        String str2 = new StringBuilder("ja").append("va").toString();
+        System.out.println(str2.intern() == str2);
     }
 }

de44eb65524dd09abf535e405e3bb7240b0d43fe
liubingguang <liubingguang>
2017-09-11 15:07:36 +0800

    线程池

---
+++ src/main/java/threadpool/CustomThread.java
@@ -0,0 +1,21 @@
+package threadpool;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 自定义线程
+ * @author LBG - 2017/9/8 0008
+ */
+public class CustomThread extends Thread {
+
+    private static final String DEFAULT_NAME = "MyBrokerThread";
+    private static final AtomicInteger created = new AtomicInteger();
+
+    public CustomThread(Runnable target) {
+        this(target, DEFAULT_NAME);
+    }
+
+    public CustomThread(Runnable target, String name) {
+        super(target, name + "-" + created.incrementAndGet());
+    }
+}

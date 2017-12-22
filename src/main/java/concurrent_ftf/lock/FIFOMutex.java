0621faa5e21ff94fbb81fb173f04c9b5271def61
liubingguang <liubingguang>
2017-12-22 15:10:08 +0800

    LockSupport

---
+++ src/main/java/concurrent_ftf/lock/FIFOMutex.java
@@ -0,0 +1,40 @@
+package concurrent_ftf.lock;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 描述
+ *
+ * @author LBG - 2017/12/22 0022
+ */
+public class FIFOMutex {
+
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+     private final Queue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
+
+      public void lock() {
+   boolean wasInterrupted = false;
+   Thread current = Thread.currentThread();
+   waiters.add(current);
+
+   // Block while not first in queue or cannot acquire lock
+   while (waiters.peek() != current ||
+          !locked.compareAndSet(false, true)) {
+      LockSupport.park(this);
+      if (Thread.interrupted()) // ignore interrupts while waiting
+        wasInterrupted = true;
+   }
+
+   waiters.remove();
+   if (wasInterrupted)          // reassert interrupt status on exit
+      current.interrupt();
+ }
+
+      public void unlock() {
+   locked.set(false);
+   LockSupport.unpark(waiters.peek());
+ }
+}

a3a000a80950bd49dfdf2c269dca250fb7685a51
liubingguang <liubingguang>
2018-01-05 19:31:01 +0800

    soa server invocation

--- src/main/java/com/somelogs/soa/annotation/JSONParam.java
+++ src/main/java/com/somelogs/soa/annotation/JSONParam.java
@@ -3,7 +3,7 @@ package com.somelogs.soa.annotation;
 import java.lang.annotation.*;
 
 /**
- * the annotation represents argument a JSON parameter,
+ * the annotation represents argument is a JSON parameter,
  * and then custom argument resolver will deserialize
  * the parameter to specified Java bean.
  *

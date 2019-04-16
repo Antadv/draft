27430eb87945ceca71522c7a9c62c80179c08ab7
liu.bingguang <liu.bingguang@chinaredstar.com>
2019-04-16 11:45:21 +0800

    四种类型的 Exchange

--- src/main/java/com/somelogs/distribution/rabbitmq/RabbitConsumer.java
+++ src/main/java/com/somelogs/distribution/rabbitmq/RabbitConsumer.java
@@ -13,12 +13,14 @@ import java.util.concurrent.TimeoutException;
  */
 public class RabbitConsumer {
 
-    private static final String QUEUE_NAME = "queue_demo";
-    private static final String IP_ADDRES = "127.0.0.1";
+    private static final String DIRECT_QUEUE_NAME = "queue_demo";
+    private static final String FANOUT_QUEUE_NAME = "fanout_queue_demo";
+    private static final String TOPIC_QUEUE_NAME = "topic_queue_demo";
+    private static final String IP_ADDRESS = "127.0.0.1";
     private static final int PORT = 5672;
 
     public static void main(String[] args) {
-        Address[] addresses = new Address[]{new Address(IP_ADDRES, PORT)};
+        Address[] addresses = new Address[]{new Address(IP_ADDRESS, PORT)};
         ConnectionFactory factory = new ConnectionFactory();
         factory.setUsername("guest");
         factory.setPassword("guest");
@@ -29,10 +31,12 @@ public class RabbitConsumer {
             final Channel channel = connection.createChannel();
             // 设置客户端最多接受未被ack消息的个数
             channel.basicQos(64);
-
             Consumer consumer = new DefaultConsumer(channel) {
                 @Override
-                public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                public void handleDelivery(String consumerTag,
+                                           Envelope envelope,
+                                           AMQP.BasicProperties properties,
+                                           byte[] body) throws IOException {
                     System.out.println("recv msg：" + new String(body));
                     try {
                         TimeUnit.SECONDS.sleep(1);
@@ -42,7 +46,7 @@ public class RabbitConsumer {
                     channel.basicAck(envelope.getDeliveryTag(), false);
                 }
             };
-            channel.basicConsume(QUEUE_NAME, consumer);
+            channel.basicConsume(TOPIC_QUEUE_NAME, consumer);
 
             // 等待回调函数执行完毕之后，关闭资源
             TimeUnit.SECONDS.sleep(5);
--- src/main/java/com/somelogs/distribution/rabbitmq/RabbitProducer.java
+++ src/main/java/com/somelogs/distribution/rabbitmq/RabbitProducer.java
@@ -6,6 +6,8 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.MessageProperties;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.TimeoutException;
 
 /**
@@ -22,32 +24,146 @@ public class RabbitProducer {
     private static final int PORT = 5672; // RabbitMQ default port
 
     public static void main(String[] args) {
+        Connection connection = getConnection();
+        //directExchange(connection);
+        //fanoutExchange(connection);
+        topicExchange(connection);
+    }
+
+    private static Connection getConnection() {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost(IP_ADDRESS);
         factory.setPort(PORT);
         factory.setUsername("guest");
         factory.setPassword("guest");
+        try {
+            return factory.newConnection();
+        } catch (IOException | TimeoutException e) {
+            throw new RuntimeException("new connection error:" + e.getMessage());
+        }
+    }
 
-        // 创建连接
-        try (Connection connection = factory.newConnection()) {
+    /**
+     * direct 类型的 Exchange：只有 RoutingKey 和 BindingKey 完全匹配才能正确路由消息
+     */
+    private static void directExchange(Connection connection) {
+        Channel channel = null;
+        try {
             // 创建通道
-            Channel channel = connection.createChannel();
+            channel = connection.createChannel();
             // 创建一个 type = "direct"、持久化的、非自动删除的交换器
             // exchangeDeclare(String exchange, String type, boolean durable, boolean autoDelete, Map<String, Object> arguments)
             channel.exchangeDeclare(EXCHANGE_NAME, "direct", true, false, null);
             channel.queueDeclare(QUEUE_NAME, true, false, false, null);
             // 将交换器与队列通过路由键绑定
+            // 其实这里需要的是BindingKey, 只不过这里的 BindingKey 和 RoutingKey 是同一个东西
             channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
 
-            // 发送消息
-            String message = "Hello World";
+            String message = "direct Hello World " + new SimpleDateFormat("mm:ss").format(new Date());
+            // 发送消息的时候，需要的是 RoutingKey
             channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
-
-            // 关闭资源
-            channel.close();
-        } catch (TimeoutException | IOException e) {
-            e.printStackTrace();
+        } catch(Exception e) {
+            throw new RuntimeException("exchange error:" + e.getMessage());
+        } finally {
+            try {
+                // 关闭资源
+                if (channel != null) {
+                    channel.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch(Exception e) {
+                System.out.println("close resource exception:" + e.getMessage());
+            }
         }
+    }
 
+    /**
+     * fanout 类型的 Exchange：所有发送到该交换器的消息路由到所有与该交换器绑定的队列（无视 BindingKey）
+     */
+    private static void fanoutExchange(Connection connection) {
+        String exchangeName = "fanout_exchange_demo";
+        String queueName = "fanout_queue_demo";
+        String bindingKey = "binding_key_fanout_demo";
+        Channel channel = null;
+        try {
+            // 创建通道
+            channel = connection.createChannel();
+            // 创建交换器
+            channel.exchangeDeclare(exchangeName, "fanout", true, false, null);
+            // 创建队列
+            channel.queueDeclare(queueName, true, false, false, null);
+            // 将交换器与队列通过路由键绑定
+            channel.queueBind(queueName, exchangeName, bindingKey);
+            // 发送消息的时候，需要的是 RoutingKey
+            // 为了测试 fanout 类型交换器，RoutingKey 和 BindingKey 设成不一致
+            String message = "fanout Hello World " + new SimpleDateFormat("mm:ss").format(new Date());
+            channel.basicPublish(exchangeName, ROUTING_KEY, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+        } catch(Exception e) {
+            throw new RuntimeException("exchange error:" + e);
+        } finally {
+            try {
+                // 关闭资源
+                if (channel != null) {
+                    channel.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch(Exception e) {
+                System.out.println("close resource exception:" + e.getMessage());
+            }
+        }
+    }
+
+    /**
+     * topic 类型 Exchange：有如下约定
+     * RoutingKey：点号“.” 分隔的字符串，每段字符串称为单词，如com.rabbitmq.client
+     * BindingKey：也是点号“.” 分隔的字符串；
+     *              还可以存在两种特殊字符串“*”, "#"
+     *              "*" 匹配一个单词
+     *              "#" 匹配多个单词（可以是零个）
+     */
+    private static void topicExchange(Connection connection) {
+        String exchangeName = "topic_exchange_demo";
+        String queueName = "topic_queue_demo";
+
+        //String bindingKey = "*.*.client";
+        //String routingKey = "com.rabbitmq.client"; // 匹配
+        //String routingKey = "rabbitmq.client"; // 不匹配
+
+        String bindingKey = "com.#";
+        //String routingKey = "com.rabbit.client"; // 匹配
+        String routingKey = "com"; // 匹配
+
+        Channel channel = null;
+        try {
+            // 创建通道
+            channel = connection.createChannel();
+            // 创建交换器
+            channel.exchangeDeclare(exchangeName, "topic", true, false, null);
+            // 创建队列
+            channel.queueDeclare(queueName, true, false, false, null);
+            // 将交换器与队列通过路由键绑定
+            channel.queueBind(queueName, exchangeName, bindingKey);
+            // 发送消息的时候，需要的是 RoutingKey
+            String message = "topic Hello World " + new SimpleDateFormat("mm:ss").format(new Date());
+            channel.basicPublish(exchangeName, routingKey, MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
+        } catch(Exception e) {
+            throw new RuntimeException("exchange error:" + e);
+        } finally {
+            try {
+                // 关闭资源
+                if (channel != null) {
+                    channel.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch(Exception e) {
+                System.out.println("close resource exception:" + e.getMessage());
+            }
+        }
     }
 }

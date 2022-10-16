10f18430a6de728c7b6ba3fa7c95d5ada8c6ad57
柳丙广 <liu.bingguang@redstarclouds.com>
2022-10-16 16:56:37 +0800

    netty hello world

---
+++ src/main/java/com/somelogs/netty/eventloop/EventLoopServer.java
@@ -0,0 +1,16 @@
+package com.somelogs.netty.eventloop;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 描述
+ *
+ * @author LBG - 2022/10/16
+ */
+@Slf4j
+public class EventLoopServer {
+
+	public static void main(String[] args) {
+		log.info("------2");
+	}
+}
